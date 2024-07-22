425af227561e3830ae28ac69876287debddaf543
KrishRamani2 <krishramani02@gmail.com>
2024-07-22 23:17:26 +0530

    Update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,100 +1,122 @@
 "use client";
-
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Select,SelectGroup,SelectItem,SelectLabel,SelectTrigger,SelectValue } from "@/components/ui/select";
+import { Select, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import { SelectContent } from "@radix-ui/react-select";
 import { motion } from "framer-motion";
-import { FaPhoneAlt,FaEnvelope,FaMapMarkedAlt } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa6";
+import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa";
+import Link from 'next/link';
 
-const info=[{
-  icon:<FaPhoneAlt />,
-  title:'Phone',
-  description:'(+91) 7977713644',
-},{
-  icon:<FaEnvelope/>,
-  title:'Email',
-  description:'krishramani02@gmail.com',
-},{
-  icon:<FaMapMarkedAlt />,
-  title:'Location',
-  description:'Mumbai,Maharashtra India',
-},]
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: 'Phone',
+    description: '(+91) 7977713644',
+    link: 'tel:+917977713644'
+  },
+  {
+    icon: <FaEnvelope />,
+    title: 'Email',
+    description: 'krishramani02@gmail.com',
+    link: 'mailto:krishramani02@gmail.com'
+  },
+  {
+    icon: <FaWhatsapp />,
+    title: 'Whatsapp',
+    description: '(+91) 7977713644',
+    link: 'https://wa.me/917977713644'
+  },
+  {
+    icon: <FaMapMarkedAlt />,
+    title: 'Location',
+    description: 'Mumbai,Maharashtra India',
+    link:"https://www.google.com/maps/place/Mumbai,+Maharashtra/@19.0822507,72.8811862,11z/data=!3m1!4b1!4m6!3m5!1s0x3be7c6306644edc1:0x5da4ed8f8d648c69!8m2!3d19.0759837!4d72.8776559!16zL20vMDR2bXA?entry=ttu"
+  },
+];
 
 const Contact = () => {
   return (
     <motion.section
-    initial={{opacity:0}}
-    animate={{opacity:1 , transition:{delay:2.4,duration:0.4,ease:"easeIn"}}}
-    className="py-6"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }}
+      className="py-6"
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
-        <div className="xl:h-[54%] order-2 xl:order-none ">
-          <form action="" className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-            <h3 className="text-4xl text-accent">
-              Let{"'"}s work together 
-            </h3>
-            <p className="text-white/60">For innovative web solutions, reach out today!</p>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              <Input type="firstname"
-              placeholder="Firstname"
-              />
-              <Input type="lastname"
-              placeholder="Lastname"
-              />
-              <Input type="email"
-              placeholder="Email"
-              />
-              <Input type="phone"
-              placeholder="Phone"
-              />
-            </div>
-            <div>
-            <Select>
-              <SelectTrigger className="w-full">
-                <SelectValue placeholder = "Select a Service" /> 
-              </SelectTrigger>
-              <SelectContent className="">
-                <SelectGroup className="bg-black w-[335px]   xl:w-[480px] text-3xl"> 
-                  <SelectLabel>
-                    Select a Service</SelectLabel>
-                    <SelectItem value="est">
-                      Web Developement
-                    </SelectItem>
-                    <SelectItem value="cst">
-                      App Developement
-                    </SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-            <Textarea className="h-[200px] mt-3 " placeholder="Type your message here." />
-            <Button size="md" className="mt-5">Send Message</Button>
-            </div>
-          </form>
-          
+          <div className="xl:h-[54%] order-2 xl:order-none ">
+            <form action="" className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+              <h3 className="text-4xl text-accent">
+                Let{"'"}s work together
+              </h3>
+              <p className="text-white/60">For innovative web solutions, reach out today!</p>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input type="firstname"
+                  placeholder="Firstname"
+                />
+                <Input type="lastname"
+                  placeholder="Lastname"
+                />
+                <Input type="email"
+                  placeholder="Email"
+                />
+                <Input type="phone"
+                  placeholder="Phone"
+                />
+              </div>
+              <div>
+                <Select>
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Select a Service" />
+                  </SelectTrigger>
+                  <SelectContent className="">
+                    <SelectGroup className="bg-black w-[335px] xl:w-[480px] text-3xl">
+                      <SelectLabel>
+                        Select a Service</SelectLabel>
+                      <SelectItem value="est">
+                        Web Development
+                      </SelectItem>
+                      <SelectItem value="cst">
+                        App Development
+                      </SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                <Textarea className="h-[200px] mt-3 " placeholder="Type your message here." />
+                <Button size="md" className="mt-5">Send Message</Button>
+              </div>
+            </form>
+          </div>
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0 ">
+            <ul className="flex flex-col mb-[25px]">
+              {info.map((item, index) => {
+                return (
+                  <li key={index} className="flex items-center gap-6">
+                    {item.link ? (
+                      <Link href={item.link} target={item.title !== 'Phone' ? "_blank" : "_self"} rel="noopener noreferrer">
+                        <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center xl:mb-[28px] cursor-pointer hover:bg-accent hover:text-[#27272c] transition-colors duration-300">
+                          <div className="text-[28px]">{item.icon}</div>
+                        </div>
+                      </Link>
+                    ) : (
+                      <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center xl:mb-[28px]">
+                        <div className="text-[28px]">{item.icon}</div>
+                      </div>
+                    )}
+                    <div className="">
+                      <p className="xl:mb-[28px] text-white/60">{item.title}</p>
+                      <h3 className="xl:mb-[28px] xl:mt-[-25px] text-xl">{item.description}</h3>
+                    </div>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
         </div>
-        <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0 ">
-          <ul className="flex flex-col mb-[25px]">
-            {info.map((item,index)=>{
-              return(
-              <li key={index} className="flex items-center gap-6">
-                <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center xl:mb-[28px]">
-                  <div className="text-[28px] ">{item.icon}</div>
-                </div>
-                <div className="">
-                  <p className="xl:mb-[28px] text-white/60">{item.title}</p>
-                  <h3 className="xl:mb-[28px] xl:mt-[-25px] text-xl">{item.description}</h3>
-                </div>
-              </li>              )
-            })}
-          </ul>
-        </div>
-      </div>
       </div>
     </motion.section>
   )
 }
 
-export default Contact
+export default Contact;
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,144 +1,169 @@
 "use client";
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import {Swiper,SwiperSlide}from "swiper/react";
-import "swiper/css"
-import {BsArrowUpRight,BsGithub} from "react-icons/bs"
-import { Tooltip,TooltipContent,TooltipProvider, TooltipTrigger} from '@radix-ui/react-tooltip';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@radix-ui/react-tooltip';
 import Link from "next/link";
 import Image from "next/image";
-import { title } from "process";
 import WorkSliderBtns from "@/components/WorkSliderBtns";
 
-
-const projects =[
+const projects = [
   {
-    num:'01',
-    category:"MagicStream:A Disney+Hotstar Clone (Frontend)",
-    title:"MagicStream: A Disney+ Hotstar Clone",
-    description:"MagicStream boasts an extensive collection of content spanning various genres, including blockbuster movies, pop-ular TV series, exclusive originals, and live sports broadcasts.",
-    stack:[{name:"Html 5"},{name:"Css 3"},{name:"JavaScript"}],
-    image:"/assets/work/Disney.png",
-    live:"/",
-    github:"https://github.com/KrishRamani2/Disney-plus-hotstar-clone",
-    },
-  {
-    num:'02',
-    category:"ZENARA Mutli Vendor E-commerce (FullStack)",
-    title:"Ecommerce Website",
-    description:"Developed ZENARA e-commerce using MERN stack, ensuring high performance and seamless user experience.Crafted a visually appealing, responsive interface for ZENARA, enhancing the overall shopping experience.Implemented advanced product filtering and user-friendly Add to Cart functionality for efficient navigation.Designed and built an admin dashboard, empowering sellers with analytics and efficient management tools.",
-    stack:[{name:"MERN Stack"}],
-    image:'/assets/work/Ecommerce.jpeg',
-    live:"/",
-    github:"https://github.com/KrishRamani2/Ecommerce_Website",
+    num: '01',
+    category: "MagicStream:A Disney+Hotstar Clone (Frontend)",
+    title: "MagicStream: A Disney+ Hotstar Clone",
+    description: "MagicStream boasts an extensive collection of content spanning various genres, including blockbuster movies, pop-ular TV series, exclusive originals, and live sports broadcasts.",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "JavaScript" }],
+    image: "/assets/work/Disney.png",
+    live: "/",
+    github: "https://github.com/KrishRamani2/Disney-plus-hotstar-clone",
   },
   {
-    num:'03',
-    category:"TinDog (FrontEnd)",
-    title:"TinDog",
-    description:"Are you tired of swiping left on dating apps that just don't get your paw-some lifestyle? Look no further than TinDog, the app that brings dogs together based on their playful personalities and tail-wagging interests.",
-    stack:[{name:"Html 5"},{name:"Css 3"},{name:"JavaScript"},{name:"BootStrap"}],
-    image:'/assets/work/TinDog.jpeg',
-    live:"/",
-    github:"https://github.com/KrishRamani2/Tin_Dog_Project",
+    num: '02',
+    category: "ZENARA Mutli Vendor E-commerce (FullStack)",
+    title: "Ecommerce Website",
+    description: "Developed ZENARA e-commerce using MERN stack, ensuring high performance and seamless user experience.Crafted a visually appealing, responsive interface for ZENARA, enhancing the overall shopping experience.Implemented advanced product filtering and user-friendly Add to Cart functionality for efficient navigation.Designed and built an admin dashboard, empowering sellers with analytics and efficient management tools.",
+    stack: [{ name: "MERN Stack" }],
+    image: '/assets/work/Ecommerce.jpeg',
+    live: "/",
+    github: "https://github.com/KrishRamani2/Ecommerce_Website",
+  },
+  {
+    num: '03',
+    category: "TinDog (FrontEnd)",
+    title: "TinDog",
+    description: "Are you tired of swiping left on dating apps that just don't get your paw-some lifestyle? Look no further than TinDog, the app that brings dogs together based on their playful personalities and tail-wagging interests.",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "JavaScript" }, { name: "BootStrap" }],
+    image: '/assets/work/TinDog.jpeg',
+    live: "/",
+    github: "https://github.com/KrishRamani2/Tin_Dog_Project",
   },
 ];
 
-
-
 const Work = () => {
-  const [project,setProject] = useState(projects[0]);
-  const handleSlideChange =(swiper: { activeIndex: any; })=>{
-    const currentIndex = swiper.activeIndex;
+  const [project, setProject] = useState(projects[0]);
+  const [imagesLoaded, setImagesLoaded] = useState({});
 
-    setProject(projects[currentIndex])
+  const handleSlideChange = (swiper) => {
+    const currentIndex = swiper.activeIndex;
+    setProject(projects[currentIndex]);
   };
+
+  const handleImageLoad = (index) => {
+    setImagesLoaded(prev => ({ ...prev, [index]: true }));
+  };
+
   return (
-    <motion.section
-    initial={{opacity:0}}
-    animate={{opacity:1 , transition:{delay:2.4,duration:0.4,ease:"easeIn"}}}
-    className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
-    >
-      <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
-         <div className=" w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
-          <div className="flex flex-col gap-[30px]">
-            <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
-              {project.num}
+    <>
+      <style jsx>{`
+        .loader {
+          border: 5px solid #f3f3f3;
+          border-top: 5px solid #3498db;
+          border-radius: 50%;
+          width: 50px;
+          height: 50px;
+          animation: spin 1s linear infinite;
+        }
+
+        @keyframes spin {
+          0% { transform: rotate(0deg); }
+          100% { transform: rotate(360deg); }
+        }
+      `}</style>
+      <motion.section
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }}
+        className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+      >
+        <div className="container mx-auto">
+          <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+            <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+              <div className="flex flex-col gap-[30px]">
+                <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                  {project.num}
+                </div>
+                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">{project.category}</h2>
+                <p className="text-white/60">{project.description}</p>
+                <ul className="flex gap-4">
+                  {project.stack.map((item, index) => {
+                    return <li key={index}
+                      className="text-xl text-accent "
+                    >{item.name}</li>;
+                  })}
+                </ul>
+                <div className="border border-white/20" />
+                <div className="flex flex-row gap-[25px]">
+                  <Link href={project.live}>
+                    <TooltipProvider delayDuration={100}>
+                      <Tooltip>
+                        <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                          <BsArrowUpRight />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p className="">Live project</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </Link>
+                  <Link href={project.github}>
+                    <TooltipProvider delayDuration={100}>
+                      <Tooltip>
+                        <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                          <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p className="">GitHub repository</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </Link>
+                </div>
+              </div>
             </div>
-            <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">{project.category}</h2>
-            <p className="text-white/60">{project.description}</p>
-            <ul className="flex gap-4">
-              {project.stack.map((item,index)=>{
-                return <li key={index}
-                className="text-xl text-accent "
-                >{item.name}</li>;
-              })}
-            </ul>
-            <div className="border border-white/20" />
-            <div className="flex flex-row gap-[25px]"><Link href={project.github}>
-            <TooltipProvider delayDuration={100}>
-              <Tooltip>
-                <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                  <BsArrowUpRight />
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p className="">Live project</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-            </Link>
-            <Link href={project.github}>
-            <TooltipProvider delayDuration={100}>
-              <Tooltip>
-                <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                  <BsGithub className="text-white text-3xl group-hover:text-accent" />
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p className="">Live project</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-            </Link>
+            <div className="w-full xl:w-[50%]">
+              <Swiper
+                spaceBetween={30}
+                slidesPerView={1}
+                className="xl:h-[520px] mb-12"
+                onSlideChange={handleSlideChange}
+              >
+                {projects.map((project, index) => {
+                  return (
+                    <SwiperSlide key={index}>
+                      <div className="h-[460px] relative group flex justify-center items-center bg-pink-50">
+                        <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                        <div className="relative w-full h-full">
+                          {!imagesLoaded[index] && (
+                            <div className="absolute inset-0 flex items-center justify-center">
+                              <div className="loader"></div>
+                            </div>
+                          )}
+                          <Image
+                            src={project.image}
+                            fill
+                            className={`object-cover transition-opacity duration-300 ${imagesLoaded[index] ? 'opacity-100' : 'opacity-0'}`}
+                            alt="project image"
+                            onLoad={() => handleImageLoad(index)}
+                          />
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                  );
+                })}
+                <WorkSliderBtns
+                  containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%-22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                  btnStyles="bg-accent hover:bg-accent-hover text-primary textr-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
+                  iconStyles="items-center"
+                />
+              </Swiper>
             </div>
           </div>
-         </div>
-         <div className="w-full xl:w-[50%]">
-          <Swiper
-          spaceBetween={30}
-          slidesPerView={1}
-          className="xl:h-[520px] mb-12"
-          onSlideChange={handleSlideChange}
-          >
-            {projects.map((project,index)=>{
-              return <SwiperSlide key={index}>
-                <div className="h-[460px] relative group flex justify-center items-center bg-pink-50">
-                  
-                  <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
-                  <div className="relative w-full h-full">
-                    <Image src={project.image} 
-                    fill
-                    className="object-cover"
-                    alt="ecommerce"
-                    loading={index === 0 ? "eager" : "lazy"}
-                    priority={index === 0}
-                    />
-                  </div>
-                </div>
-              </SwiperSlide>
-            })}
-            <WorkSliderBtns 
-            containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%-22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
-            btnStyles="bg-accent hover:bg-accent-hover text-primary textr-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
-            iconStyles="items-center"
-            />
-          </Swiper>
-         </div>
         </div>
-      </div>
-    </motion.section>
-  )
-}
+      </motion.section>
+    </>
+  );
+};
 
-export default Work
-
+export default Work;
