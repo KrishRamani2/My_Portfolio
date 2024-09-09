d114867761453249d5ad1287e218ab4966f6fca7
KrishRamani2 <krishramani02@gmail.com>
2024-09-09 21:27:14 +0530

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "../components/StairEffect"
 import Headers from "../components/Header"
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const jetBrainsmono = JetBrains_Mono({ 
   subsets: ["latin"], 
   weight: ["100","200","300","400","500","600","700","800"],
